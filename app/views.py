96eeb11e964fe51dbd578c62db1696539ebb17b5
Alex Campbell <alexmhcampbell.2@gmail.com>
2026-03-02 22:45:23 -0500

    Lab 3 is complete.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 from app import app
+from app.forms import ContactForm
+from flask_mail import Message
+from app import mail
 from flask import render_template, request, redirect, url_for, flash
 
 
@@ -16,6 +19,29 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        name    = form.name.data
+        email   = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        msg = Message(
+            subject,
+            sender=(name, email),
+            recipients=[app.config['MAIL_USERNAME']]
+        )
+        msg.body = f"From: {name} <{email}>\n\n{message}"
+        mail.send(msg)
+
+        flash('Your email was sent successfully!', 'success')
+        return redirect(url_for('home'))
+    else:
+        flash_errors(form)
+    return render_template('contact.html', form=form)
 
 
 ###
